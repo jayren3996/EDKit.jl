28cee4eb6fdac79083ff45ca9fe511341f1a2467
JayRen <jieren3996@foxmail.com>
2021-04-21 21:53:17 +0800

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,12 @@
-using Documenter
 push!(LOAD_PATH,"../src/")
 using EDKit
-
-makedocs(sitename="EDKit.jl Documentation")
+using Documenter
+makedocs(
+         sitename = "EDKit.jl",
+         modules  = [EDKit],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/jayren3996/EDKit.jl",
+)
