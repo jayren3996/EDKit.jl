705c9bb7b0edc7854d3468e92274f19e4463b5c5
JieRen <32599461+jayren3996@users.noreply.github.com>
2021-10-28 15:44:42 +0800

    update QIM

--- src/algorithms/QIM.jl
+++ src/algorithms/QIM.jl
@@ -6,18 +6,17 @@ Calculate Hamiltonian from eigen state(s).
 
 
 inner_product(v1::AbstractVector{<:Number}, v2::AbstractVector{<:Number}) = dot(v1, v2)
-inner_product(v1::AbstractMatrix{<:Number}, v2::AbstractMatrix{<:Number}) = sum(dot(v1[:, i], o * v2[:, i]) for i in 1:size(v1, 2)) / size(v1, 1)
+inner_product(v1::AbstractMatrix{<:Number}, v2::AbstractMatrix{<:Number}) = sum(dot(v1[:, i], v2[:, i]) for i in 1:size(v1, 2)) / size(v1, 1)
 
 export covmat
 function covmat(ol::AbstractVector{T}, v::AbstractVecOrMat{<:Number}) where T <: Union{<:AbstractMatrix, <:Operator}
     n = length(ol)
     vs = [oi * v for oi in ol]
-    am = [inner_product(v, vsi) for vsi in vs]
+    am = [real(inner_product(v, vsi)) for vsi in vs]
     cm = Matrix{Float64}(undef, n, n)
-
     for i=1:n
         for j=i:n
-            cm[i, j] = real(inner_product(vs[i], vs[j])) - am[i] * am[j]
+            cm[i, j] = real(inner_product(vs[i], vs[j])) - am[i] * am[j] 
         end
     end
     Hermitian(cm)
