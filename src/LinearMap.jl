f95276a3eeaedc8812ba4ed884f5d1c2e59a6dd9
JieRen <32599461+jayren3996@users.noreply.github.com>
2023-03-29 19:37:04 +0800

    Update LinearMap.jl

--- src/LinearMap.jl
+++ src/LinearMap.jl
@@ -33,7 +33,7 @@ end
 @inline size(b::DoubleBasis, i::Integer) = isone(i) ? size(b.B1, 1) : isequal(i, 2) ? size(b.B2, 2) : 1
 function copy(b::DoubleBasis)
     dgt = deepcopy(b.dgt) 
-    DoubleBasis(dgt, b.B1, b.B2, b.B)
+    DoubleBasis(dgt, copy(b.B1), copy(b.B2), b.B)
 end
 
 #-----------------------------------------------------------------------------------------------------
